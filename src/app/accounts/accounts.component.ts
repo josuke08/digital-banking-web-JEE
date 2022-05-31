0b6061eb40cc3afd06bfdf01189b7f3203bba002
Mohamed Farhi <62158040+josuke08@users.noreply.github.com>
2022-05-31 21:44:12 +0100

    Add files via upload

---
+++ src/app/accounts/accounts.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AccountsService} from "../services/accounts.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {AccountDetails} from "../model/account.model";
+
+@Component({
+  selector: 'app-accounts',
+  templateUrl: './accounts.component.html',
+  styleUrls: ['./accounts.component.css']
+})
+export class AccountsComponent implements OnInit {
+  accountFormGroup! : FormGroup;
+  operationFormGroup! : FormGroup;
+  currentPage : number = 0;
+  pageSize : number = 5;
+  accountObservable! : Observable<AccountDetails>
+  errorMessage! : string;
+
+  constructor(private fb: FormBuilder, private accountService : AccountsService) { }
+
+  ngOnInit(): void {
+    this.accountFormGroup = this.fb.group({
+      accountId: this.fb.control('')
+    });
+    this.operationFormGroup = this.fb.group({
+      operationType: this.fb.control(null),
+      amount : this.fb.control(0),
+      description: this.fb.control(null),
+      accountDestination : this.fb.control(null)
+    });
+  }
+
+  handleSearchAccount() {
+    let accountId : string = this.accountFormGroup.value.accountId;
+    this.accountObservable = this.accountService.getAccount(accountId, this.currentPage, this.pageSize).pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      })
+    );
+  }
+
+  gotoPage(page: number) {
+    this.currentPage=page;
+    this.handleSearchAccount();
+
+  }
+
+  handleAccountOperation() {
+    let accountId : string = this.accountFormGroup.value.accountId;
+    let operationType = this.operationFormGroup.value.operationType;
+    let amount : number = this.operationFormGroup.value.amount;
+    let description : string = this.operationFormGroup.value.description;
+    let accountDestination : string = this.operationFormGroup.value.accountDestination;
+    if(operationType=='DEBIT'){
+      this.accountService.debit(accountId, amount, description).subscribe({
+          next: data=>{
+
+            alert("Success debit");
+            this.operationFormGroup.reset();
+            this.handleSearchAccount();
+          },
+          error: err => {
+            alert("fail debit");
+            console.log(err.message);
+          }
+        }
+      );
+    }else if(operationType=='CREDIT'){
+
+      this.accountService.credit(accountId, amount, description).subscribe({
+          next: data=>{
+            alert("Success credit");
+            this.operationFormGroup.reset();
+            this.handleSearchAccount();
+          },
+          error: err => {
+            console.log(err.message);
+          }
+        }
+      );
+    }else if(operationType=='TRANSFER'){
+      this.accountService.transfer(accountId, accountDestination, amount, description).subscribe({
+          next: data=>{
+            alert("Success transfer");
+            this.operationFormGroup.reset();
+            this.handleSearchAccount();
+          },
+          error: err => {
+            console.log(err.message);
+          }
+        }
+      );
+    }
+  }
+}
